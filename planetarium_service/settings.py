1614016a7bbbcfcdbd301512ed32571fe449945f
Serhii <sergei.amvarcumov@gmail.com>
2026-08-17 22:12:42 +0200

    refactor(settings): update SECRET_KEY logic

--- planetarium_service/settings.py
+++ planetarium_service/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-rqan0yedg5+)vwvxup7t&d8t9asj8%=ht)kjrpm+dm&(&nw__+"
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-rqan0yedg5+)vwvxup7t&d8t9asj8%=ht)kjrpm+dm&(&nw__+")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
